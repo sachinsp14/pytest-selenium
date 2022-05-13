1a79f5ef07e2a669b0a7cbf963d6e79ce18c7c65
Sachin Siddanuru Prabhulingappa <sachinsp14@gmail.com>
2022-05-12 19:36:04 -0700

    Update test_chrome.py

--- test_chrome.py
+++ test_chrome.py
@@ -23,4 +23,4 @@ browser.maximize_window()
 @pytest.mark.browser_test
 def test_assert_empty_login_form():
     browser.get('https://myevesachinsp1651776534764.dashboard.psonar.us/login')
-    assert "Welcome to Prosimo" in browser.page_source
+    assert "Welcome to Prosimo" in browser.page_source, broswer.page_source
