29bd11dfba3282b6d3320ca813741a9a869158a1
Sachin Siddanuru Prabhulingappa <sachinsp14@gmail.com>
2022-05-12 19:11:59 -0700

    Update chrome.py

--- chrome.py
+++ chrome.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-driver.close()
+
+browser = webdriver.Firefox()
+browser.maximize_window()
+
+
+# This test will run when 'login_test' is called when invoking -m,
+# e.g. 'pytest test_custom_markers.py -m "login_test"'.
+# This test will run when 'empty_form_test' is called when invoking -m,
+# e.g. 'pytest test_custom_markers.py -m "empty_form_test"'.
+@pytest.mark.broswer_test
+def test_assert_empty_login_form():
+    browser.get(https://myevesachinsp1651776534764.dashboard.psonar.us/login)
+    assert "Welcome to Prosimo" in broswer.page_source
