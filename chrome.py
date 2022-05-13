90d279a9f417448a9c3ca008d0ece739460ba7fb
Sachin Siddanuru Prabhulingappa <sachinsp14@gmail.com>
2022-05-12 19:21:52 -0700

    Update chrome.py

--- chrome.py
+++ chrome.py
@@ -12,7 +12,7 @@ browser.maximize_window()
 # e.g. 'pytest test_custom_markers.py -m "login_test"'.
 # This test will run when 'empty_form_test' is called when invoking -m,
 # e.g. 'pytest test_custom_markers.py -m "empty_form_test"'.
-@pytest.mark.broswer_test
+@pytest.mark.browser_test
 def test_assert_empty_login_form():
     browser.get('https://myevesachinsp1651776534764.dashboard.psonar.us/login')
     assert "Welcome to Prosimo" in browser.page_source
